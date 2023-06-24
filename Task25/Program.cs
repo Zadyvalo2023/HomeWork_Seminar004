018b1d3973d1ae9bd457a5c56140f6f48fae41e6
Zadyvalo2023 <av1469400@gmail.com>
2023-06-24 17:22:00 +0300

    My home work

---
+++ Task25/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+using System;// Это директива using, которая позволяет использовать функционал из пространстваимен System
+namespace PowerExample //это определение пространства имен PowerExample, в котором содержится класс Program;
+{
+    class Program // class Program - это определение класса Program, который содержит точку входа в программу - метод Main
+    {
+
+        //это метод Main, который запускается при запуске программы
+        //Он принимает массив строк в качестве аргументов и имеет модификаторы static и void, что означает, что он является статическим методом, который не возвращает никакого значения;
+static void Main(string[] args)
+        {
+            Console.Write("Введите число A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите степень B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            double result = 1;
+            for (int i = 0; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.WriteLine($"{a} в степени {b} = {result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+//и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Задаем массив из 8 элементов
+        int[] array = new int[8];
+        Random rand = new Random();
+
+        // Инициализируем массив случайными значениями
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(100);
+        }
+
+        // Выводим элементы массива на экран
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+
+        // Ждем, пока пользователь не нажмет клавишу Enter
+        Console.ReadLine();
+    }
+}
